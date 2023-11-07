601e1fa3d85c37c47c588f463905d4e4410cf4b4
Filipovic Luka <filipovic793@gmail.com>
2023-11-07 22:59:26 +0100

    ORT2 ispit(K3) zavrsen

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -33,10 +33,10 @@ def make_an_excel_table():
     )
 
     #Prva tabela
-    worksheet.set_column("A:A", 10)
+    worksheet.set_column("A:A", 5)
     worksheet.set_column("B:B", 30)
     worksheet.set_column("G:G", 12)
-    worksheet.set_row(0, 36)
+    worksheet.set_row(0, 40)
 
     worksheet.conditional_format('A2:H8', {'type': 'cell',
                                                  'criteria': '>=',
@@ -52,7 +52,41 @@ def make_an_excel_table():
     worksheet.write('H1', 'PC', header_format)
 
     #Druga tabela
+    worksheet.set_row(11, 40)
 
+    for i in range(12, 19):
+        worksheet.merge_range(i, 2, i, 3, '')
+        worksheet.merge_range(i, 4, i, 5, '')
+        worksheet.merge_range(i, 6, i, 7, '')
+
+    worksheet.conditional_format('A13:H19', {'type': 'cell',
+                                           'criteria': '>=',
+                                           'value': 0, 'format': cell_format})
+
+    worksheet.write('A12', '#', header_format)
+    worksheet.write('B12', 'Адресе у меморији са којих је учитана адреса операнда', header_format)
+    worksheet.merge_range('C12:D12', 'Адресе у меморији са којих је учитан операнд', header_format)
+    worksheet.merge_range('E12:F12', 'Операнд', header_format)
+    worksheet.merge_range('G12:H12', 'Нови садржај регистара опште намене', header_format)
+
+    #Treca tabela
+    worksheet.set_row(22, 40)
+
+    for i in range(23, 30):
+        worksheet.merge_range(i, 7, i, 9, '')
+
+    worksheet.conditional_format('A24:J30', {'type': 'cell',
+                                             'criteria': '>=',
+                                             'value': 0, 'format': cell_format})
+
+    worksheet.write('A23', '#', header_format)
+    worksheet.write('B23', 'Меморијске адресе којима се приступа у овој фази', header_format)
+    worksheet.write('C23', 'N', header_format)
+    worksheet.write('D23', 'Z', header_format)
+    worksheet.write('E23', 'V', header_format)
+    worksheet.write('F23', 'C', header_format)
+    worksheet.write('G23', 'Акумулатор', header_format)
+    worksheet.merge_range('H23:J23', 'Нови садржај регистара и меморијских локација који су промењени у овој фази', header_format)
 
 
     workbook.close()
